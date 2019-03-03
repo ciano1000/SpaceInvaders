bd9cbeaa2c32c1ff1214fb5c65670677ed558f0d
CIAN MC SWEENEY <ciano1000@gmail.com>
2019-03-03 20:00:05 +0000

    Finished the game, added game over, scores etc

--- src/com/spaceinvaders/AlienFormation.java
+++ src/com/spaceinvaders/AlienFormation.java
@@ -8,14 +8,22 @@ public class AlienFormation {
 
     private static final int NUMALIENS = Invaders.NUMALIENS;
     private int dir = 1;
+    private int windowWidth;
 
+    private double speedMultiplier = 1.0;
     private double startX;
     private double startY;
+
+    private Image img1, img2;
 
     private boolean reverseDir = false;
 
     public AlienFormation(Image img1,Image img2, int windowWidth)
     {
+        this.windowWidth = windowWidth;
+        this.img1 = img1;
+        this.img2 = img2;
+
         startX = (windowWidth/2)*Math.random();
         startY = 50;
 
@@ -70,9 +78,40 @@ public class AlienFormation {
         }
     }
 
+    public void resetAliens()
+    {
+        startX = (windowWidth/2)*Math.random();
+        startY = 50;
+
+        for(int i = 0;i<NUMALIENS;i++) {
+            Alien alien = aliens.get(i);
+            if (i < 5)
+                alien.setPosition((startX + (75 * (i + 1))), startY);
+            if (i > 4)
+                alien.setPosition((startX + (75 * (i - 4))), startY + 75);
+            if (i > 9)
+                alien.setPosition((startX + (75 * (i - 9))), startY + 150);
+            if (i > 14)
+                alien.setPosition((startX + (75 * (i - 14))), startY + 225);
+            if (i > 19)
+                alien.setPosition((startX + (75 * (i - 19))), startY + 300);
+            if (i > 24)
+                alien.setPosition((startX + (75 * (i - 24))), startY + 375);
+
+            alien.isAlive = true;
+            alien.setxSpeed(1.0*speedMultiplier);
+        }
+
+        speedMultiplier++;
+    }
+
     public ArrayList<Alien> getAliens()
     {
         return aliens;
     }
 
+    public void resetMultiplier()
+    {
+        speedMultiplier = 1.0;
+    }
 }
--- src/com/spaceinvaders/Invaders.java
+++ src/com/spaceinvaders/Invaders.java
@@ -14,6 +14,8 @@ public class Invaders extends JFrame implements Runnable, KeyListener {
 
     private boolean isInitialised = false;
     private boolean isGameRunning = true;
+
+    private int numAliensAlive = NUMALIENS;
     private int score = 0;
     private int bestScore = 0;
 
@@ -26,7 +28,7 @@ public class Invaders extends JFrame implements Runnable, KeyListener {
     private Image bulletImage = bulletIcon.getImage();
 
     private static final Dimension WindowSize = new Dimension(800,800);
-    private static String workingDir = "C:\\Users\\ciano\\IdeaProjects\\SpaceInvaders\\src\\com\\spaceinvaders\\ct255-images";
+    private static String workingDir = "C:\\Users\\Cian\\IdeaProjects\\SpaceInvaders\\src\\com\\spaceinvaders\\ct255-images";
     private BufferStrategy strategy;
 
     public Invaders()
@@ -80,6 +82,7 @@ public class Invaders extends JFrame implements Runnable, KeyListener {
             }catch (InterruptedException e){}
             formation.move();
             bulletCollision();
+            alienCollision();
 
             this.repaint();
         }
@@ -107,6 +110,14 @@ public class Invaders extends JFrame implements Runnable, KeyListener {
                 break;
             default:
                 break;
+        }
+
+        if(!isGameRunning)
+        {
+            if(keyCode == KeyEvent.VK_ENTER)
+            {
+                resetGame();
+            }
         }
     }
 
@@ -151,13 +162,60 @@ public class Invaders extends JFrame implements Runnable, KeyListener {
                        if(((x1<x2 && x1+w1>x2)||(x2<x1&&x2+w2>x1)) &&((y1<y2 && y1+h1>y2)||(y2<y1&&y2+h2>y1)))
                        {
                            alien.isAlive = false;
+                           numAliensAlive--;
                            score+=50;
                            iterator.remove();
+                           System.out.println((numAliensAlive));
                            break;
                        }
                    }
                }
            }
+        }
+        if(numAliensAlive == 0)
+        {
+            System.out.println("Resetting");
+            formation.resetAliens();
+            aliens = formation.getAliens();
+            numAliensAlive = 30;
+        }
+    }
+
+    private void handleGameOver()
+    {
+        if(score>bestScore)
+            bestScore =score;
+
+        isGameRunning = false;
+    }
+
+    private void resetGame()
+    {
+        isGameRunning = true;
+        formation.resetMultiplier();
+        formation.resetAliens();
+        player.setPosition(363,750);
+        score =0;
+    }
+
+    private void alienCollision()
+    {
+        double w1 = 50,h1 = 32;
+        double w2 = 54,h2 =32;
+        double x2 = player.getX(), y2 = player.getY();
+        for(int i =0;i<NUMALIENS;i++)
+        {
+            Alien alien = aliens.get(i);
+            double x1 = alien.getX(),y1 = alien.getY();
+            if(y1>800)
+            {
+                handleGameOver();
+                break;
+            }
+            if(((x1<x2 && x1+w1>x2)||(x2<x1&&x2+w2>x1)) &&((y1<y2 && y1+h1>y2)||(y2<y1&&y2+h2>y1)))
+            {
+                handleGameOver();
+            }
         }
     }
 
@@ -170,27 +228,40 @@ public class Invaders extends JFrame implements Runnable, KeyListener {
         g.setColor(Color.BLACK);
         g.fillRect(0,0,WindowSize.width,WindowSize.height);
 
-        for (Alien alien:
-                aliens) {
-            if(alien.isAlive)
-                alien.paint(g);
-        }
-
-        if(bullets.size()>0)
+        if(isGameRunning)
         {
-            for(int i=0;i<bullets.size();i++)
-            {
-                bullets.get(i).paint(g);
+            for (Alien alien:
+                    aliens) {
+                if(alien.isAlive)
+                    alien.paint(g);
             }
-        }
 
-        //string drawing
-        g.setColor(Color.GREEN);
-        Font courier = new Font ("Courier", Font.BOLD, 24);
-        g.setFont(courier);
-        g.drawString("Score: "+score, 263, 75);
-        g.drawString("High Score: "+bestScore,463,75);
-        player.paint(g);
+            if(bullets.size()>0)
+            {
+                for(int i=0;i<bullets.size();i++)
+                {
+                    bullets.get(i).paint(g);
+                }
+            }
+
+            //string drawing
+            g.setColor(Color.GREEN);
+            Font courier = new Font ("Courier", Font.BOLD, 24);
+            g.setFont(courier);
+            g.drawString("Score: "+score, 263, 75);
+            g.drawString("High Score: "+bestScore,463,75);
+            player.paint(g);
+        }
+        else
+        {
+            g.setColor(Color.GREEN);
+            Font gameOver = new Font("Courier",Font.BOLD,48);
+            g.setFont(gameOver);
+            g.drawString("Game Over",275,400);
+            g.drawString("Score: "+score+" Best Score: "+bestScore,150,500);
+            g.drawString("ENTER to restart",200,700);
+
+        }
         strategy.show();
     }
     public static void main(String[] args)
